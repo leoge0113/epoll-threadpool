574a0056f86b521a1fb1ed15ff9b61173a513ad3
LeoGe <361350493@qq.com>
2016-11-10 17:04:42 +0800

    write file for logging

---
+++ file_operations.h
@@ -0,0 +1,15 @@
+/*
+ * file_operations.h
+ *
+ *  Created on: 2016.11
+ *      Author: Leo
+ */
+
+#ifndef FILE_OPERATIONS_H_
+#define FILE_OPERATIONS_H_
+#include "g_net_global.h"
+
+int read_file_to_buff(char *file_path, unsigned long len, char *data);
+
+
+#endif /* FILE_OPERATIONS_H_ */
